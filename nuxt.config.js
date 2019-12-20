8364b00e941bdbdd3695c0a523186b4dcb8e6ae1
albsbz <best.shop.bot@gmail.com>
2019-12-20 16:30:54 +0200

    start commit

---
+++ nuxt.config.js
@@ -0,0 +1,56 @@
+
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+
+  head: {
+    title: 'nuxt-app',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  modules: [
+    '@nuxtjs/axios',
+    
+    // Simple usage
+    'nuxt-express-module',
+
+    // With options
+    ['nuxt-express-module', { /* module options */ }],
+  ],
+
+  axios: {
+    // proxyHeaders: false
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+  plugins: [
+    { src: '@/plugins/element-ui', mode: 'client' }
+  ]
+}
