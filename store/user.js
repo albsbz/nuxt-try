1971ef8408b945b04adc70a5965d274940f14596
albsbz <best.shop.bot@gmail.com>
2020-02-12 09:21:00 +0200

    clear

--- store/user.js
+++ store/user.js
@@ -1,4 +1,4 @@
-// import { getAnalytics } from "../api/controllers/post.controller"
+
 
 export const actions={
     async fetchAdminUsers({commit}){
@@ -9,14 +9,7 @@ export const actions={
             throw e
         }
     },
-    // async fetch({commit}){
-    //     try {
-    //         return await this.$axios.$get('/api/user')
-    //     } catch (e) {
-    //         commit('setError', e, {root:true})
-    //         throw e
-    //     }
-    // },
+    
     async delete({commit}, id){
         try {
             return await this.$axios.$delete(`/api/user/admin/${id}`)
@@ -33,43 +26,7 @@ export const actions={
             throw e
         }
     },
-    async update({commit}, {text, id}){
-        // try {
-        //     return await this.$axios.$put(`/api/post/admin/${id}`, {text})
-        // } catch (e) {
-        //     commit('setError', e, {root:true})
-        //     throw e
-        // }
-    },
-    async create({commit}, {title, text, image}){
-        // try {
-        //     const fd=new FormData()
-        //     fd.append('title', title)
-        //     fd.append('text', text)
-        //     fd.append('image', image, image.name)
-        //     return await this.$axios.$post('/api/post/admin', fd)
-            
-        // } catch (e){
-        //     commit('setError', e, {root:true})
-        //     throw e
-        // }
-    },
-    async fetchAdminById({commit}, id){
-        // try {
-        //     return await this.$axios.$get(`/api/post/admin/${id}`)
-        // } catch (e) {
-        //     commit('setError', e, {root:true})
-        //     throw e
-        // }
-    },
-    // async fetchById({commit}, id){
-    //     try {
-    //         return await this.$axios.$get(`/api/user/${id}`)
-    //     } catch (e) {
-    //         commit('setError', e, {root:true})
-    //         throw e
-    //     }
-    // },
+
     async fetchCurrentUser({commit, rootState}){
         try {
             return await this.$axios.$get(`/api/user/${rootState.auth.currentUser.id}`)
